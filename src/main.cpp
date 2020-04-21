b61ee800ed6d76cd3a9eaca7975b9f7b257e3f15
sbalymo <sbalymov@yandex.ru>
2020-04-21 20:41:08 +0300

    ((

--- src/main.cpp
+++ src/main.cpp
@@ -14,7 +14,7 @@ long previousMillis_read = 0;
 long time_click = 0;
 float temp;
 uint8_t gister = 0.3; //гистеререзис
-uint8_t step = 0;
+volatile uint8_t step = 0;
 const uint8_t heater = 13; // Пин подключение нагревателя
 const uint8_t btn_on = 2;  //Пин подключения кнопки
 const uint8_t buz = 3;     //Пин для подключения пищалки
@@ -27,43 +27,7 @@ bool var = false;       // Для  для включения сервы посл
 bool varHeatTo = false; //перемннная для функции нагреть до
 
 Heater heat(heater);
-/*
-//включение тена
-void controlHeater(bool ON)
-{
-  if (ON)
-    digitalWrite(heater, HIGH);
-  else if (!ON)
-    digitalWrite(heater, LOW);
-}
 
-// функция поддерживания температуры // Сделать класс нагревателя
-void step_heating(uint8_t setpoint)
-{
-  if (temp < setpoint - gister)
-  {
-    controlHeater(1);
-  }
-  else if (temp > setpoint + gister)
-  {
-    controlHeater(0);
-  }
-}
-
-//Нагреть до
-void heatTo(uint8_t setpoint)
-{
-  if (temp < setpoint - gister && varHeatTo)
-  {
-    controlHeater(1);
-  }
-  else if (temp > setpoint + gister)
-  {
-    controlHeater(0);
-    varHeatTo = false;
-  }
-}
-*/
 //включение выключение движка
 void controlMotor(bool ON)
 {
@@ -94,7 +58,6 @@ bool btContinue()
     return true;
   }
   return false;
-
 }
 //выход из шага
 void exitInStep()
@@ -103,8 +66,8 @@ void exitInStep()
   var = false;
   varHeatTo = true;
   step++;
-  heat.oNoff(false); // на выходе из каждго шага выключаем Тен, очень сомнительно 
-                    // по функционалу но вроде безопаснее
+  heat.oNoff(false); // на выходе из каждго шага выключаем Тен, очень сомнительно
+                     // по функционалу но вроде безопаснее
 }
 
 void setup()
@@ -134,6 +97,8 @@ void loop()
     previousMillis = currentTime;
   }
 
+  
+
   switch (step)
   {
 
@@ -156,7 +121,6 @@ void loop()
     {
       Serial.println(F("Step 1 done"));
       exitInStep();
-      
     }
     break;
 
@@ -182,7 +146,7 @@ void loop()
       }
     }
     break;
-//Шаг 3 вносим фермент и время t
+    //Шаг 3 вносим фермент и время t
   case 3: //таймер 20 минут
     btContinue();
     if (var)
@@ -199,22 +163,23 @@ void loop()
     }
     break;
 
-///ПРОВЕРИТЬ плохо отработоло!!!!!! Ошибка что на это шаге время берем в секундах а нам его не кто не дал)))
   case 4: //Жднем нажатие кнопки для включение сервы на 30 секунд
-    btContinue();
+    if (btContinue())
+      start_time = getTimeInSec();
+
     if (var)
     {
-       controlMotor(true);                             // нажали на кнопку включем движок
-       if (currentTime - previousMillis11 > 1000) // текущие - предыдущие
+      controlMotor(true);                        // нажали на кнопку включем движок
+      if (currentTime - previousMillis11 > 1000) // текущие - предыдущие
       {
-      if (getTimeInSec() - start_time >= 30)
-      {
-        controlMotor(false);
-        exitInStep();
-        Serial.println(F("Step 4 done"));
+        if (getTimeInSec() - start_time >= 30)
+        {
+          controlMotor(false);
+          exitInStep();
+          Serial.println(F("Step 4 done"));
+        }
+        previousMillis11 = currentTime;
       }
-      previousMillis11 = currentTime;
-     }
     }
     break;
 
@@ -223,25 +188,26 @@ void loop()
     btContinue();
     if (var)
     {
-      controlMotor(true); // нажали на кнопку включем движок
-      heat.heatTo(&temp,setTemp1, &varHeatTo);//и нагреваем t
-   
-    if (currentTime - previousMillis11 > 1000) 
-     { if (getTimeInMin() - start_time >= 1)
+      controlMotor(true);                       // нажали на кнопку включем движок
+      heat.heatTo(&temp, setTemp1, &varHeatTo); //и нагреваем t
+
+      if (currentTime - previousMillis11 > 1000)
       {
-        controlMotor(false);
-        buzzer(); //через 15 минут вырбуаем движок и пищим бузером
-        exitInStep();
-        Serial.println(F("Step 5 done"));
+        if (getTimeInMin() - start_time >= 1)
+        {
+          controlMotor(false);
+          buzzer(); //через 15 минут вырбуаем движок и пищим бузером
+          exitInStep();
+          Serial.println(F("Step 5 done"));
+        }
+        previousMillis11 = currentTime;
       }
-      previousMillis11 = currentTime;
-    }
     }
     break;
-   
+
   // нагрев до T  и t держим температуру
   case 6:
-   heat.tempMaint(&temp,setTemp1,1);//греем до 50 и ждем 1.5 часа
+    heat.tempMaint(&temp, setTemp1, 1); //греем до 50 и ждем 1.5 часа
     if (getTimeInMin() - start_time >= 2)
     {
       exitInStep();
@@ -250,4 +216,8 @@ void loop()
     }
     break;
   }
+
+
+
+
 }
